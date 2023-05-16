603a2c86b69ad8b0ab3f87186b754474826c2610
Evelyn Russo <evelyn.russo94@gmail.com>
2023-05-16 11:22:11 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up to date with 'Mod-3/master'.  Changes to be committed: 	modified:   .firebase/hosting.ZGlzdFxwcnVlYmEy.cache 	modified:   src/app/dashboardproyectos/dashboardproyectos.component.css 	modified:   src/app/dashboardproyectos/dashboardproyectos.component.html 	modified:   src/app/dashboardproyectos/dashboardproyectos.component.ts

--- src/app/dashboardproyectos/dashboardproyectos.component.ts
+++ src/app/dashboardproyectos/dashboardproyectos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DatosService } from '../servicios/datos.service';
+import { Proyecto } from '../models/proyecto';
+import { ProyectoService } from '../servicios/proyecto.service';
 
 
 @Component({
@@ -11,14 +13,29 @@ export class DashboardproyectosComponent implements OnInit {
   titulo: string = '';
   realizacion: string = '';
   Descripcion: string = '';
+  proyectos: Proyecto[]=[];
 
-  constructor(private datosService: DatosService) { }
+  constructor(private proyService: ProyectoService) { }
 
   ngOnInit(): void {
-    this.datosService.getDatos().subscribe(data => {
-      this.titulo=data.titulo;
-      this.realizacion=data.realizacion;
-      this.Descripcion=data.Descripcion;
-    });
+    this.cargarProyecto();
   }
+
+  //llamar a los metodos
+    public cargarProyecto():void{ //sin retorno, solo carga datos
+      this.proyService.List().subscribe(data => {this.proyectos=data});
+    }
+
+    public borrar(id:number){
+      if(id != undefined){
+        this.proyService.eliminar(id).subscribe(
+          data =>{
+            // alert("Experiencia eliminada correctamente)
+            this.cargarProyecto();
+          }, err =>{
+            alert("No se pudo elmiminar la experiencia")
+          }
+        )
+      }
+    }
   }
