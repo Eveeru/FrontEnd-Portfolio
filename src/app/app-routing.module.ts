4e0537574ad65c5d9f452a572cedaf1e2a6c0370
Evelyn Russo <evelyn.russo94@gmail.com>
2023-05-16 12:09:44 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up to date with 'Mod-3/master'.  Changes to be committed: 	modified:   .firebase/hosting.ZGlzdFxwcnVlYmEy.cache 	modified:   src/app/app-routing.module.ts 	modified:   src/app/dashboard/dashboard.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { IntroComponent } from './intro/intro.component';
-import { LoginComponent } from './modals/login/login.component';
 
 
 const routes: Routes = [
   {path:'**', component:IntroComponent},
-  //{path:'login', component:LoginComponent},
   {path:'dashboard', component:DashboardComponent},
-  {path:'', redirectTo:'/intro', pathMatch:'full'}
+  //{path:'', redirectTo:'/intro', pathMatch:'full'}
 ];
 
 @NgModule({
